98d5e48f63b42a24e7df2fee8a709bf2194233eb
Mike Sinkula <mike@mikesinkula.com>
2015-05-03 13:56:16 -0700

    New!

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+    <!-- Begin Content -->
+    <div id="content">
+    <h2>404 Error:</h2>
+    <p>The page you are looking for does not exist... Try a search?</p>
+	<?php get_search_form(); ?>
+    <small>404.php</small>
+    </div>
+    <!-- End Content -->
+    
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
