92a35d6545ba9329635190e33c5303e2633c7abc
Mike Sinkula <mike@mikesinkula.com>
2015-05-03 13:44:44 -0700

    Making' it purty.

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,6 @@
    
     <!-- Begin Sub-Navigation -->
     <div id="sub-navigation" class="widget">
-    
     <?php if (is_page()) : // if we are in "pages"... ?>
     <h2 class="sub-navigation-title">
     <?php echo get_the_title($post->post_parent); // ...get the gateway page title ?></h2>
@@ -21,15 +20,12 @@
     
     ?></ul>
     <?php endif; // end if we are in "pages" ?>
-    
     <?php if (!(is_page())) : // if we are not in "pages"... ?>
     <h2 class="sub-navigation-title">Blog</h2>
     <ul class="sub-navigation-items"><?php wp_list_categories(array('title_li' => __(''))); // ...list the categories with no title ?></ul>
     <?php endif; ?>
-    
     </div>
     <!-- End Sub-Navigation -->
-
     
 </div>
 <!-- End Sidebar -->
